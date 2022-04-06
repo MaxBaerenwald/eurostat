63a9c6661636d550302c8297a43a760804fbdd81
Max <maxbaerenwal@t-online.de>
2022-04-06 14:47:18 +0200

    Created test for data (germany)

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 # This is a sample Python script.
+import numpy
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import eurostat
 import datetime
 
-
 if __name__ == '__main__':
-
     df = eurostat.get_data_df("ei_bsco_m")
     df[["indic", "s_adj", "unit", "geo\\time"]].describe()
-    df = pd.DataFrame(df)
+    test_value_of_germany = df[df["indic"].eq("BS-CSMCI") & df["unit"].eq("BAL") & df["s_adj"].eq("NSA") & df["geo\\time"].eq("DE")]
+    print(test_value_of_germany.keys())
+    print(test_value_of_germany)
 
-    print(df)
-    """
-    de_cci = df[(df["country"] == "DE") & (df["indic"] == "BS-CSMCI") & (df["s_adj"]
-                                                                         == "NSA")]
-    de_cci = de_cci[[c for c in de_cci.columns if isinstance(c, datetime) and
-                     (c.year == 2020 or c.year == 2019)]]
-    de_cci.index = ["Consumer confidence indicator"]
-    de_cci.transpose()[::-1].plot.bar(figsize=(16, 9))
-"""
-
-
-
+    # df = pd.DataFrame(df).describe()
+    # df = df.cumsum()
+    # plt.figure()
+    # df.plot()
