6551509c1bbe8dc1f7e08d26094446fc6702ef4f
Max <maxbaerenwal@t-online.de>
2022-04-13 12:14:38 +0200

    Updated

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import datetime
 if __name__ == '__main__':
     df = eurostat.get_data_df("ei_bsco_m")
     df[["indic", "s_adj", "unit", "geo\\time"]].describe()
-    test_value_of_germany = df[df["indic"].eq("BS-CSMCI") & df["unit"].eq("BAL") & df["s_adj"].eq("NSA") & df["geo\\time"].eq("DE")]
+    test_value_of_germany = df[
+        df["indic"].eq("BS-CSMCI") & df["unit"].eq("BAL") & df["s_adj"].eq("NSA") & df["geo\\time"].eq("DE")]
     print(test_value_of_germany.keys())
     test_value_of_germany = test_value_of_germany.drop(columns=['indic', 's_adj', 'unit'])
     print(test_value_of_germany)
